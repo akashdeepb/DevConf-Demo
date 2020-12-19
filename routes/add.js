ca69812567c290cbf89c36083f51a4487955a80e
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-12-18 21:47:52 -0800

    Created Route

--- routes/add.js
+++ routes/add.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',function(req,res){
-    res.send("hello");
+    res.status(200).json({message:"OK"});
 });
 
 module.exports = router;
