57544e730d8d64f73917ed38946b0dd68acd8371
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2020-12-18 21:38:14 -0800

    Created Route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ let app = express();
 
 let PORT = 3000;    // PORT YOU WANT TO RUN YOUR SERVER ON
 
+// GET Request to Server /
+app.get('/',function(req,res){
+    res.send("Hi");
+});
 
 app.listen(PORT,function(){
     console.log("Server running on PORT : " + PORT);
